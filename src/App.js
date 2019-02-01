02a82f9a0ab34361b8d736cc2ca4dee34993c8e1
billy <billyliadphillips@gmail.com>
2019-02-01 11:12:31 +0000

    added in input comp with styles

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Header } from './components/common';
+import React, { Component } from "react";
+import { View } from "react-native";
+import firebase from "firebase";
+
+import { Header } from "./components/common";
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentDidMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBoW52cJTQzky9qsKPN3I3NiyZIfJSSm-g",
+      authDomain: "authnative-3e589.firebaseapp.com",
+      databaseURL: "https://authnative-3e589.firebaseio.com",
+      projectId: "authnative-3e589",
+      storageBucket: "authnative-3e589.appspot.com",
+      messagingSenderId: "952041897244"
+    });
+  }
   render() {
     return (
-    <View>
-        <Header headerText="Authentication"/>
-        <Text>Test!!!!</Text>
-    </View>
-    )
+      <View>
+        <Header headerText="Authentication" />
+        <LoginForm />
+      </View>
+    );
   }
 }
-
 
 export default App;
